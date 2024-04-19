ce0ffed8a4b884ea655ee870b7f281963487bf1f
yecq <yecq_82@hotmail.com>
2024-04-19 16:06:04 +0800

    add simple sample of swift

---
+++ test/test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by 叶常青 on 2024/4/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
