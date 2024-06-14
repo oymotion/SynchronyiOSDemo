8814544c35bffb1d2f84eebe9be63a21cee3fce5
yecq <yecq_82@hotmail.com>
2024-06-14 09:34:24 +0800

    fix bugs

--- test/sensor.xcframework/ios-arm64_x86_64-maccatalyst/sensor.framework/Versions/A/Headers/sensor.h
+++ test/sensor.xcframework/ios-arm64_x86_64-maccatalyst/sensor.framework/Versions/A/Headers/sensor.h
@@ -8,6 +8,12 @@
 #ifndef sensorOC_Bridge_h
 #define sensorOC_Bridge_h
 
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double sensorVersionNumber = 1;
+
+
+FOUNDATION_EXPORT const unsigned char sensorVersionString[] = "1.0";
 
 #import <sensor/sensorProfile.h>
 #import <sensor/SensorController.h>
